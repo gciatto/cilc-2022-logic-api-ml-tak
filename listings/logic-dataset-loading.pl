6c4b8d83b2eaea45d5323b12bc6dc9d53de06ef7
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-06-27 11:54:11 +0200

    chore: add figures, scripts, tables, and references from paper

---
+++ listings/logic-dataset-loading.pl
@@ -0,0 +1,13 @@
+/* attributes definition here */
+
+% dataset definition
+iris(5.1, 3.2, 1.4, 0.2, setosa).
+iris(4.9, 3, 1.7, 1.2, versicolor).
+iris(5.9, 3.4, 1.1, 0.9, virginica).
+/*... other entries here...*/
+
+% reading schema from theory
+iris_schema(S) :- theory_to_schema(S).
+
+% reading dataset from theory            
+iris_dataset(D) :- iris_schema(S), theory_to_dataset(S, D).
---
+++ listings/training.pl
@@ -0,0 +1,37 @@
+/* Hyper paramenters */
+hidden_layers([10]). hidden_layers([20, 10]). 
+hidden_layers([30, 20, 10]).
+
+/* Learning paramenters */
+max_epochs(30). max_epochs(50).
+batch_size(32). batch_size(16).
+learning_rate(0.01). learning_rate(0.1).
+loss(cross_entropy).
+
+/* Workflow paramenters */
+target_performance(0.90). test_percentage(0.2).
+
+/* Generates all hyper & learning params combinations */
+params(
+  [hidden_layers(H)],
+  [iterations(X), learning_rate(Y), batch_size(Z), loss(L)]
+) :- hidden_layers(H), max_epochs(X), learning_rate(Y),
+     batch_size(Z), loss(L).
+
+/* Generates and trains all Predictors for the given Dataset and Schema, 
+whose Performance is at least target_performance. */
+model_selection(Dataset, Schema, Predictor, Transform, Performance) :-
+     test_percentage(R), target_performance(T),
+     random_split(Dataset, R, TrainSet, TestSet),
+     preprocessing_pipeline(TrainSet, Schema, Transform),
+     transform(TrainSet, Transform, ProcessedTrainSet),
+     params(HyperParams, LearnParams),
+     train_cv(ProcessedTrainSet, HyperParams, LearnParams, P),
+     P >= T,
+     multi_layer_perceptron(4, HyperParams, NN),
+     train(NN, TrainingSet, LearnParams, Predictor),
+     transform(TrainSet, Transform, ProcessedTestSet),
+     test(NN, ProcessedTestSet, Performance).
+
+/* Example of training query: */
+?- iris_dataset(D), iris_schema(S), (*@\label{line:training-query}@*)model_selection(D, S, P, _, A).
