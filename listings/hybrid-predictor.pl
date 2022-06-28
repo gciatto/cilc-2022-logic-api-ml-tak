2d1f6d2ed129e9a0a347a1c1fd1a406971aa9e3f
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-06-28 11:21:32 +0200

    feat!: main content

--- listings/hybrid-predictor.pl
+++ listings/hybrid-predictor.pl
@@ -1,10 +1,10 @@
 /* assumption: */
-:- (*@\label{line:hybrid-predictor-setup}@*)iris_dataset(D), iris_schema(S), model_selection(D, S, N, T, _), !,
-    assert(iris_nn(N, T)).
+:- (*@\label{line:hybrid-predictor-setup}@*)iris_dataset(D), iris_schema(S),
+   model_selection(D, S, N, T, _), !, assert(iris_nn(N, T)).
 
-/* hybrid iris classifier */ 
+/* hybrid iris classifier */
 iris(SL, SW, PL, PW, Species) :-
    iris_nn(Network, Transformation),
-   transform([SL, SW, PL, PW] , Transformation, ActualX), 
+   transform([SL, SW, PL, PW] , Transformation, ActualX),
    predict(Network, ActualX, Y),
    classify(Y, argmax, [setosa, versicolor, virginica], Species).
--- listings/training.pl
+++ listings/training.pl
@@ -1,12 +1,9 @@
 /* Hyper paramenters */
-hidden_layers([10]). hidden_layers([20, 10]). 
-hidden_layers([30, 20, 10]).
+hidden_layers([10]). hidden_layers([20, 10]).
 
 /* Learning paramenters */
-max_epochs(30). max_epochs(50).
-batch_size(32). batch_size(16).
-learning_rate(0.01). learning_rate(0.1).
-loss(cross_entropy).
+max_epochs(30). max_epochs(50). batch_size(32).
+batch_size(16). learning_rate(0.01). loss(cross_entropy).
 
 /* Workflow paramenters */
 target_performance(0.90). test_percentage(0.2).
@@ -16,22 +13,17 @@ params(
   [hidden_layers(H)],
   [iterations(X), learning_rate(Y), batch_size(Z), loss(L)]
 ) :- hidden_layers(H), max_epochs(X), learning_rate(Y),
-     batch_size(Z), loss(L).
-
-/* Generates and trains all Predictors for the given Dataset and Schema, 
-whose Performance is at least target_performance. */
-model_selection(Dataset, Schema, Predictor, Transform, Performance) :-
-     test_percentage(R), target_performance(T),
+     batch_size(Z), loss(L).(*@\framebreak@*)
+% Generates and trains all Predictors for the given Dataset
+% and Schema, having at least a given performance.
+model_selection(Dataset, Schema, Predictor) :-
+     test_percentage(R), target_performance(Threshold),
      random_split(Dataset, R, TrainSet, TestSet),
-     preprocessing_pipeline(TrainSet, Schema, Transform),
-     transform(TrainSet, Transform, ProcessedTrainSet),
      params(HyperParams, LearnParams),
-     train_cv(ProcessedTrainSet, HyperParams, LearnParams, P),
-     P >= T,
+     train_cv(TrainSet, HyperParams, LearnParams, Performance),
+     Performance >= Threshold,
      multi_layer_perceptron(4, HyperParams, NN),
-     train(NN, TrainingSet, LearnParams, Predictor),
-     transform(TrainSet, Transform, ProcessedTestSet),
-     test(NN, ProcessedTestSet, Performance).
+     train(NN, TrainSet, LearnParams, Predictor).
 
 /* Example of training query: */
-?- iris_dataset(D), iris_schema(S), (*@\label{line:training-query}@*)model_selection(D, S, P, _, A).
+?- iris_dataset(D), iris_schema(S), (*@\label{line:training-query}@*)model_selection(D, S, P)
